b1a48e77d52765f6b97b5cd8f560bf2a7ebb1982
caiyaguang <1274694503@qq.com>
2017-05-09 16:04:26 +0800

    ceshi

--- data/config/url.php
+++ data/config/url.php
@@ -1,7 +1,7 @@
 <?php 
 return array (
-  'URL_MODEL' => 0,
-  'URL_HTML_SUFFIX' => '',
+  'URL_MODEL' => 1,
+  'URL_HTML_SUFFIX' => '.html',
   'URL_PATHINFO_DEPR' => '/',
   'URL_ROUTER_ON' => true,
   'URL_ROUTE_RULES' => 
